4d939e376455e8c03de2f40e01d53506954ef7d9
Junior-Jurado <juniorjurado2004@gmail.com>
2024-11-05 17:13:53 -0500

    Siguiente commit

---
+++ backend/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const logger = require('morgan');
+const cors = require('cors');
+const passport = require('passport');
+
+/*
+*   RUTAS
+*/
+const users = require('./routes')
+
+const port = process.env.PORT || 3000;
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(cors());
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
+app.disable('x-powered-by');
+
+app.set('port', port);
+
+// Llamando a las rutas
+users(app);
+
+server.listen(3000, '10.31.23.117' || 'localhost', function() {
+    console.log("API projects " + process.pid + " iniciada ... \nEn el puerto " + port)
+});
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(err.status || 500).send(err.stack);
+});
+
+module.exports = {
+    app: app,
+    server: server
+}
