bd243b9aae28137c33e1380c5bccc53e1125e8f0
Junior-Jurado <juniorjurado2004@gmail.com>
2024-11-11 11:45:06 -0500

    testea Nicolas

--- frontend/app.js
+++ frontend/app.js
@@ -1,2 +1,25 @@
-import './styles/app.css'
-import './styles/homeScreen.css'
+require('./styles/app.css');
+// require('./styles/homeScreen.css');
+require('./styles/app.css');  // This requires the styles to be bundled or preprocessed
+
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById('login').addEventListener('click', login);
+});
+
+function login() {
+    console.log("login function called");  // Verificar si la función se llama
+
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+
+    console.log('Email:', email);
+    console.log('Password:', password);
+
+    setTimeout(function() {
+        window.location.href = 'HomeScreen.html';
+    }, 15000); 
+}
+
+
+
